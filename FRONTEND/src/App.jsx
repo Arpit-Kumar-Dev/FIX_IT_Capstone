1d72cf05971951288eac7d1103a4a2d9ec322029
Arpitkumar0909 <kumararpit56744@gmail.com>
2024-12-29 21:41:23 +0530

    seting up the fronend

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          WELLCOME BOSS CORDER
+      </div>
+      
+    </>
+  )
+}
+
+export default App
